e05ab10a9210ca915eac422bb499284be5118ad3
Ricardo Alves <45212494+Trash666Kill@users.noreply.github.com>
2026-08-27 21:16:17 -0300

    update

---
+++ srv52342/root/.services/firewall/c.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Close on any error (Optional)
+#set -e
+
+NIC0=br_vlan710
+
+ct160716() {
+    445() {
+        #SMB - CIFS
+        # DNAT Rules
+        nft add rule inet firelux prerouting iifname "$NIC0" ip protocol tcp tcp dport 445 dnat to 10.0.10.51:445
+
+        # Forward Rules
+        nft add rule inet firelux forward ip protocol tcp tcp dport 445 accept
+    }
+
+    # Call
+    445
+}
+
+ct615237() {
+    6600() {
+        #Music Streaming - MPD Server with USB DAC passthrough
+        # DNAT Rules
+        nft add rule inet firelux prerouting iifname "$NIC0" ip protocol tcp tcp dport 6600 dnat to 10.0.10.52:6600
+
+        # Forward Rules
+        nft add rule inet firelux forward ip protocol tcp tcp dport 6600 accept
+    }
+
+    5644() {
+        #Music Streaming - MyMPD Web Client
+        # DNAT Rules
+        nft add rule inet firelux prerouting iifname "$NIC0" ip protocol tcp tcp dport 5644 dnat to 10.0.10.52:5644
+
+        # Forward Rules
+        nft add rule inet firelux forward ip protocol tcp tcp dport 5644 accept
+    }
+
+    # Call
+    6600
+    5644
+}
+
+# Main function to orchestrate the setup
+main() {
+    RULES="
+    ct160716
+    ct615237
+    "
+
+    for RULE in $RULES
+    do
+        $RULE
+        sleep 4
+    done
+}
+
+# Execute main function
+main
